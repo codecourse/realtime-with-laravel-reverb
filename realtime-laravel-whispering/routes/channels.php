abcf4c35a778510bf9f47175d6f6572f282a4d3d
Alex Garrett-Smith <alex@codecourse.com>
2024-06-26 12:18:22 +0100

    Episode 17

--- realtime-laravel-whispering/routes/channels.php
+++ realtime-laravel-whispering/routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('app', function (User $user) {
+    return true;
 });
