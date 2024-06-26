ce3cb97a3bcfe3e1ec5773351784e4a175a1d6f8
Alex Garrett-Smith <alex@codecourse.com>
2024-06-26 09:11:52 +0100

    Episode 04

--- realtime-laravel-basics/resources/views/dashboard.blade.php
+++ realtime-laravel-basics/resources/views/dashboard.blade.php
@@ -8,7 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div
+                    class="p-6 text-gray-900"
+                    x-init="
+                        Echo.channel('chat')
+                            .listen('Example', (event) => {
+                                console.log(event)
+                            })
+                    "
+                >
                     {{ __("You're logged in!") }}
                 </div>
             </div>
