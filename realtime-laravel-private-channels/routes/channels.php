065622eae8845f7a960364ef60664f5bfa9342e1
Alex Garrett-Smith <alex@codecourse.com>
2024-06-26 10:13:07 +0100

    Episode 09

--- realtime-laravel-private-channels/routes/channels.php
+++ realtime-laravel-private-channels/routes/channels.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+Broadcast::channel('users.{id}', function (User $user, $id) {
     return (int) $user->id === (int) $id;
 });
