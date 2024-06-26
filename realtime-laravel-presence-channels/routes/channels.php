62c6b7378c5562c556db1bd41be2c572928843fb
Alex Garrett-Smith <alex@codecourse.com>
2024-06-26 11:01:28 +0100

    Episode 14

--- realtime-laravel-presence-channels/routes/channels.php
+++ realtime-laravel-presence-channels/routes/channels.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('room.{roomId}', function (User $user, $roomId) {
+    return $user->only('id', 'name');
 });
