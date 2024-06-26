c65aaeb9319d0140e194dd6127229975d3b7a2e8
Alex Garrett-Smith <alex@codecourse.com>
2024-06-26 10:56:18 +0100

    Episode 13

--- realtime-laravel-presence-channels/routes/web.php
+++ realtime-laravel-presence-channels/routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Room;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/room/{room}', function (Room $room) {
+    return view('room', [
+        'room' => $room
+    ]);
+})->middleware(['auth', 'verified'])->name('room');;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
